e52bc3ca55ef5e86d9f44793cfbc5a7c74b99e56
adaniellova20-source <adaniellova20@gmail.com>
2025-11-21 15:09:32 -0600

    Create auth.py

---
+++ app/routes/auth.py
@@ -0,0 +1,34 @@
+from http import HTTPStatus
+import os
+
+from flask import Blueprint, request, jsonify
+from marshmallow import ValidationError
+from dotenv import load_dotenv
+
+from flask_jwt_extended import create_access_token
+
+from app.schemas import UserSchema
+
+auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+user_schema = UserSchema()
+
+load_dotenv()
+
+@auth_bp.post('/login')
+def get_token():
+    try:
+        data = user_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify({'message': str(e)}), HTTPStatus.BAD_REQUEST
+
+    print(request.headers['Authorization'])
+
+    env_user = os.getenv('USER')
+    env_password = os.getenv('PASSWORD')
+
+    if env_user != data['username'] or env_password != data['password']:
+        return jsonify({"message": "Invalid credentials"}), HTTPStatus.UNAUTHORIZED
+
+    token = create_access_token(identity=data['username'])
+    return token, HTTPStatus.OK
