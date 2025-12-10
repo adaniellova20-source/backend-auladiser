36d6e37db08f52b76b19ce8d88898a106f6a2339
adaniellova20-source <adaniellova20@gmail.com>
2025-12-10 09:38:50 -0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,4 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+  SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+  
+  JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+  JWT_ALGORITHM = "HS256"
+  JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
