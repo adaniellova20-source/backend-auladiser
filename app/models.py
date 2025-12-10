71e93806f06a9fdc753bf544822e70ad57188db9
adaniellova20-source <adaniellova20@gmail.com>
2025-12-10 09:47:09 -0600

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from app.extensions import db
 
 class Customer(db.Model):
@@ -13,3 +15,21 @@ class Customer(db.Model):
     created_at = db.Column(db.DateTime, default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
     is_active = db.Column(db.Boolean, default=True)
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError("La contraseña no se puede leer.")
+
+    @password.setter
+    def password(self, plaintext_password):
+        self.password_hash = generate_password_hash(plaintext_password)
+
+    def check_password(self, plaintext_password):
+        return check_password_hash(self.password_hash, plaintext_password)
