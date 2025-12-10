561de3b45970c214d01563791c6a2076c0ebddf8
adaniellova20-source <adaniellova20@gmail.com>
2025-12-10 09:02:11 -0600

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,4 @@
 from app.routes.customers import customers_bp
-from app.routes.auth import auth_bp
 
 def register_routes(app):
     app.register_blueprint(customers_bp)
-    app.register_blueprint(auth_bp)
