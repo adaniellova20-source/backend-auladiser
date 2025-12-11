476bc74a58238e569384a48bdd1393a9bbbf49a7
adaniellova20-source <adaniellova20@gmail.com>
2025-12-10 21:19:03 -0600

    Remove unnecessary comment in conftest.py
    Remove comment about keeping app_context alive during tests

---
+++ tests/conftest.py
@@ -0,0 +1,55 @@
+import os
+from datetime import date
+
+import pytest
+from flask.testing import FlaskClient
+
+os.environ["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+
+from app import create_app
+from app.extensions import db
+from app.models import Customer
+
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update(
+        TESTING=True,
+        SQLALCHEMY_SESSION_OPTIONS={
+            "expire_on_commit": False
+        }
+    )
+
+    ctx = app.app_context()
+    ctx.push()
+
+    db.drop_all()
+    db.create_all()
+
+    yield app
+
+    db.session.remove()
+    db.drop_all()
+    ctx.pop()
+
+
+@pytest.fixture()
+def client(app) -> FlaskClient:
+    return app.test_client()
+
+
+@pytest.fixture()
+def sample_customer(app):
+    customer = Customer(
+        name="Juan",
+        lastname="Perez",
+        category="A",
+        email="juan.perez@example.com",
+        age=30,
+        url="https://example.com",
+        birthday=date(1997, 5, 10),
+    )
+    db.session.add(customer)
+    db.session.commit()
+    return customer
