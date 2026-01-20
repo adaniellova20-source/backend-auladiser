9dfdc13c66669110f657ef717d6f99e784828244
adaniellova20-source <adaniellova20@gmail.com>
2026-01-19 20:43:45 -0600

    Add blank line for code readability

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from app.config import Config
 from app.extensions import db
 from app.routes import register_routes
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
