ffda7782f59073a0e0388530607885ae35deb399
adaniellova20-source <adaniellova20@gmail.com>
2026-03-19 18:07:54 -0600

    Update models.py

--- app/models.py
+++ app/models.py
@@ -20,6 +20,7 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(120), nullable=False)
 
     @property
     def password(self):
