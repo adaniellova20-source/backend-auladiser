d9a87ca43bd07e984364a65428c4f665242bb488
adaniellova20-source <adaniellova20@gmail.com>
2026-04-09 14:35:45 -0600

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from marshmallow import Schema, fields, validate, validates, ValidationError
+from marshmallow import Schema, fields, validate
 
 class CustomerSchema(Schema):
     id = fields.Integer(dump_only=True)
